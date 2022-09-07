4e977a0747c0d03873dcea55c5d3cb4bf80a5c32
25156136 <25156136@CN-SHG02-016>
2022-09-07 14:24:20 +0100

    Add project files.

---
+++ 2.4.1.7/_2.4.1.7.py
@@ -0,0 +1,13 @@
+john=3
+mary=5
+adam=6
+print("john", "mary", "adam", sep=", ")
+total_apples=john+mary+adam
+print(total_apples)
+print("")
+amps=3
+volts=5
+print("3 Amps", "5 Volts", sep=", ")
+power=amps*volts
+print(power, "Watts")
+print("")
